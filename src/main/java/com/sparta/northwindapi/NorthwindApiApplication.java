af4c1403f8b99b4ab3adabe65120ddf2a36545b7
Andrei Hirleata <hirleata.andrei@gmail.com>
2022-10-05 14:50:50 +0100

    initial commit

---
+++ src/main/java/com/sparta/northwindapi/NorthwindApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.northwindapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthwindApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/northwindapi/NorthwindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.northwindapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

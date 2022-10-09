028097057239695eaf0464c553b0be75489c4ed8
BahadirUK <100964237+BnBuk@users.noreply.github.com>
2022-10-09 23:24:27 +0100

    DAO & DTO updated

--- src/main/java/com/sparta/northwindapi/Repo/RegionRepository.java
+++ src/main/java/com/sparta/northwindapi/Repo/RegionRepository.java
@@ -3,5 +3,9 @@ package com.sparta.northwindapi.repo;
 import com.sparta.northwindapi.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+    List<Region> findByRegionDesc(String regionName);
 }
